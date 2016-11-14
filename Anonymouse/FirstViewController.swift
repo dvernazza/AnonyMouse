521d88049b1f5b2517c36e3fb0c5f0730e1c080b
Elon University <dvernazza@carpenter-04.elon.edu>
2016-11-14 16:18:19 -0500

    Initial Commit

---
+++ Anonymouse/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Anonymouse
+//
+//  Created by Dominic Vernazza on 11/14/16.
+//  Copyright © 2016 Dominic Vernazza. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Anonymouse/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Anonymouse
+//
+//  Created by Dominic Vernazza on 11/14/16.
+//  Copyright © 2016 Dominic Vernazza. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
